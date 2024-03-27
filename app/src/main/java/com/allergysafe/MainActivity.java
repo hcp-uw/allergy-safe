7f2d0492e6ba8fba4b7e2e87db916d4b00abb1cb
Tej Bhan <tejbhan@live.com>
2024-03-27 02:45:19 -0700

    Added allergy info page main functions.

--- app/src/main/java/com/allergysafe/MainActivity.java
+++ app/src/main/java/com/allergysafe/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonAllergyInfo;
